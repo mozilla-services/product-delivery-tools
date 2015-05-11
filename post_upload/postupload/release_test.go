0cdc54f790aab768ca137bdb1bf1618aa9d1823b
Jeremiah Orem <oremj@oremj.com>
2015-05-11 09:17:41 -0700

    release: test ToLatest

---
+++ post_upload/postupload/release_test.go
@@ -0,0 +1,62 @@
+package postupload
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestCopier struct {
+	Src    string
+	Dest   string
+	Copied bool
+}
+
+func (c *TestCopier) Copy(src, dest string) error {
+	c.Src = src
+	c.Dest = dest
+	c.Copied = true
+	return nil
+}
+
+func (c *TestCopier) Reset() {
+	c.Src = ""
+	c.Dest = ""
+	c.Copied = false
+}
+
+func NewTestRelease() (*Release, *TestCopier) {
+	copier := new(TestCopier)
+	return &Release{
+		FtpPrefix: "prefix/ftp",
+		PvtPrefix: "prefix/pvt",
+		FtpCopier: copier,
+		PvtCopier: copier,
+	}, copier
+}
+
+func TestReleaseToLatest(t *testing.T) {
+	assert := assert.New(t)
+	rel, copier := NewTestRelease()
+	rel.Branch = "l10n"
+	rel.SourceDir = "/tmp/src"
+	rel.BuildDir = "build-dir"
+	rel.TinderboxBuildsDir = "tbox-win32"
+
+	err := rel.ToLatest("/tmp/src/subdir/file")
+	assert.Nil(err)
+	assert.Equal("prefix/ftp/latest-l10n/build-dir", copier.Dest)
+
+	err = rel.ToLatest("/etc/passwd")
+	assert.NotNil(err)
+
+	copier.Reset()
+	err = rel.ToLatest("/tmp/src/path1/path2/test.xpi")
+	assert.Nil(err)
+	assert.Equal("prefix/ftp/latest-l10n/build-dir/path1/path2", copier.Dest)
+
+	copier.Reset()
+	err = rel.ToLatest("/tmp/src/mar.exe")
+	assert.Nil(err)
+	assert.Equal("prefix/ftp/latest-l10n/build-dir/mar-tools/win32", copier.Dest)
+}
