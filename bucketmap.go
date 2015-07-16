9e365232c8f9df4eefc45e718683898d43404b1c
Jeremiah Orem <oremj@oremj.com>
2015-07-16 14:05:14 -0700

    move to 2 bucket model

--- bucketmap.go
+++ bucketmap.go
@@ -19,21 +19,7 @@ type BucketMount struct {
 var ProdBucketMap = BucketMap{
 	Default: "archive",
 	Mounts: []BucketMount{
-		BucketMount{"pub/calendar/", "calendar"},
-		BucketMount{"pub/firefox/bundles/", "hg-bundles"},
-		BucketMount{"pub/firefox/try-builds/", "firefox-try"},
+		BucketMount{"pub/firefox/try-builds/", "archive"},
 		BucketMount{"pub/firefox/", "firefox"},
-		BucketMount{"pub/labs/", "labs"},
-		BucketMount{"pub/mobile/try-builds/", "firefox-android-try"},
-		BucketMount{"pub/mobile/", "firefox-android"},
-		BucketMount{"pub/nspr/", "security"},
-		BucketMount{"pub/opus/", "opus"},
-		BucketMount{"pub/seamonkey/", "seamonkey"},
-		BucketMount{"pub/security/", "security"},
-		BucketMount{"pub/thunderbird/try-builds/", "thunderbird-try"},
-		BucketMount{"pub/thunderbird/", "thunderbird"},
-		BucketMount{"pub/xulrunner/try-builds/", "xulrunner-try"},
-		BucketMount{"pub/xulrunner/", "xulrunner"},
-		BucketMount{"pub/webtools/", "webtools"},
 	},
 }
