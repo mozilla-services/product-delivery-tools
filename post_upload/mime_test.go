a4a360a13b6c00e3a578d47f3a0ef02003e32d11
Jeremiah Orem <oremj@oremj.com>
2015-08-12 14:06:26 -0700

    post_upload: add contenttype guessing

---
+++ post_upload/mime_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContentType(t *testing.T) {
+	files := [][]string{
+		[]string{"/foo/bar/firefox.mar", "application/octet-stream"},
+		[]string{"/foo/bar/firefox.dmg", "application/x-apple-diskimage"},
+		[]string{"/foo/bar/firefox.png", "image/png"},
+		[]string{"/foo/bar/firefox.unknown", "application/octet-stream"},
+	}
+
+	for _, f := range files {
+		assert.Equal(t, f[1], ContentType(f[0]))
+	}
+}
